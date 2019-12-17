7470c3c6466428b1b2e6a8a4e08120139b4b3a39
aleja653 <56899349+aleja653@users.noreply.github.com>
2019-12-16 22:40:10 -0500

    Se hizo header y foofter, se crearon los servicios, incluir logos en assets, se creo modelos y servicios de front

--- frontCastle/src/app/app.component.ts
+++ frontCastle/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CompartidoService } from './servicios/compartido.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontCastle';
+  title = 'FrontCastle';
+
+  usuarioLogueado = false;
+
+  constructor (
+    private _compartidoService:CompartidoService,
+    private _router: Router
+  ){
+  if(localStorage.getItem("sesion") != null){
+    this.usuarioLogueado = true;
+  }
+  this._compartidoService.logueEmitido.subscribe (
+      usuarioLogueado =>{
+      this.usuarioLogueado = usuarioLogueado;
+    }
+  )
+}
+cerrarSesion(){
+  this.usuarioLogueado = false;
+  localStorage.removeItem("sesion");
+  localStorage.removeItem("playlist");
+  this._router.navigate(['/'])
+}
 }
---
+++ frontCastle/src/app/app.routing.ts
@@ -0,0 +1,18 @@
+import {ModuleWithProviders} from '@angular/core'; 
+//modulos y funcionalidades de router
+import {Routes,RouterModule} from '@angular/router';
+
+import {ContentIndexComponent} from 
+'./componentes/content-index/content-index.component';
+import {LoginComponent} from
+ './componentes/login/login.component';
+import {HeaderComponent} from 
+'./componentes/header/header.component';
+const appRoutes: Routes = [
+    {path: 'login', component:LoginComponent},
+    {path: '', component:ContentIndexComponent},
+    {path:'menu',component:HeaderComponent}
+];
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders 
+= RouterModule.forRoot(appRoutes); 
